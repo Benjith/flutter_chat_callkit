0acc4b3221763bf360c70ac330237642ba3a6478
Laison-zoondia <laison@zoondia.net>
2025-11-11 16:07:45 +0530

    Add phone emoji to incoming call message

--- lib/inherited/agora_chat_manager.dart
+++ lib/inherited/agora_chat_manager.dart
@@ -336,7 +336,7 @@ class AgoraChatManager {
       sType = 'video';
     }
     final msg = ChatMessage.createTxtSendMessage(
-        targetId: userId, content: 'Incoming call: $sType');
+        targetId: userId, content: '📞 Incoming call: $sType');
     Map attr = {
       kMsgType: kMsgTypeValue,
       kAction: kInviteAction,
